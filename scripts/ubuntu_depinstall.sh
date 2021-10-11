e97aa72da92af8c69cc90b502c513e1c748ab72c
StrangestMan <88807074+StrangestMan@users.noreply.github.com>
2021-10-11 09:23:06 -0500

    Create ubuntu_depinstall.sh
    Added a dependency install file

---
+++ scripts/ubuntu_depinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This file will install the required dependencies for Debian/Ubuntu based systems
+# No need to run this as root, as sudo will do it for you
+
+sudo apt install build-essential qtbase5-dev qt5-qmake
